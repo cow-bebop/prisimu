67b19b740b3476c44d1e2ef293e3169391e35afa
けお <o.kengo1987@gmail.com>
2021-08-05 02:36:52 +0900

    upd: ピュアメモリーピース計算の実装

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 (function () {
   'use strict';
 
-  const MEMORYPIECE_HARD = 0.5;
+  // メモリーピース
+  const KAKURITSU_OF_MEMORYPIECE = 0.5;
   const hardStageCount = document.querySelector('.js-hardStageCount');
   let hardStageCountVal = hardStageCount.value;
 
   const stoneCount = document.querySelector('.js-stoneCount');
-  let stoneCountVal = '';
+  let stoneCountVal = stoneCount.value;
 
   const soubi = document.querySelector('.js-soubi');
   let soubiFlg = soubi.checked;
@@ -24,30 +25,50 @@
 
   const NeedMemoPi = document.querySelector('.js-needMemoPi');
 
-  NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * MEMORYPIECE_HARD), 10);
+  // ピュアメモリーピース
+  const KAKURITSU_OF_PUREMEMORYPIECE = 0.5;
 
+  const stageClearCount = document.querySelector('.js-stageClearCount');
+  let stageClearCountVal = stageClearCount.value;
+
+  const pureMemoPiCount = document.querySelector('.js-pureMemoPiCount');
+  let pureMemoPiCountVal = pureMemoPiCount.value;
+  const NeedPureMemoPi = document.querySelector('.js-needPureMemoPi');
+
+  NeedPureMemoPi.textContent = Math.round((50 - pureMemoPiCountVal) / (stageClearCountVal * KAKURITSU_OF_PUREMEMORYPIECE), 10);
+
+  NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * KAKURITSU_OF_MEMORYPIECE), 10);
+
+  // メモリーピースのイベント
   // キャラの星数が変わった時
   starCount.addEventListener('change', (event) => {
     charaMemoPi = charaMemoPiObj[event.target.value];
-    NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * MEMORYPIECE_HARD), 10);
+    NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * KAKURITSU_OF_MEMORYPIECE), 10);
   })
 
   // ハードステージ数が変わった時
   hardStageCount.addEventListener('change', (event) => {
     hardStageCountVal = event.target.value;
-    NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * MEMORYPIECE_HARD), 10);
+    NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * KAKURITSU_OF_MEMORYPIECE), 10);
   })
 
   // 専用装備の有無が変わった時
   soubi.addEventListener('change', (event) => {
     soubiFlg = !soubiFlg;
     soubiNeedsMemoPi = soubiFlg ? 50 : 0;
-    NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * MEMORYPIECE_HARD), 10);
+    NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * KAKURITSU_OF_MEMORYPIECE), 10);
   })
 
   // メモリーピース数が変わった時
   stoneCount.addEventListener('change', (event) => {
     stoneCountVal = event.target.value;
-    NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * MEMORYPIECE_HARD), 10);
+    NeedMemoPi.textContent = Math.round((charaMemoPi - stoneCountVal - soubiNeedsMemoPi) / (hardStageCountVal * 3 * KAKURITSU_OF_MEMORYPIECE), 10);
+  })
+
+  // ピュアメモリーピースのイベント
+  // ステージを回る回数を変えた時
+  stageClearCount.addEventListener('change', (event) => {
+    stageClearCountVal = event.target.value;
+    NeedPureMemoPi.textContent = Math.round((50 - pureMemoPiCountVal) / (stageClearCountVal * KAKURITSU_OF_PUREMEMORYPIECE), 10);
   })
 }());
